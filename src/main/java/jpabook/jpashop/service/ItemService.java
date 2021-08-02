a21f4ac91bb1334c4e08c5badb3d2280714e76fe
henoh <henoh@nkia.co.kr>
2021-08-02 14:11:40 +0900

    재고 관련 서비스 및 테스트 추가

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    //TODO: 권한 위임관련 설정만 있는 서비스의 경우 리포지터리 직접 접근 고려려
+   @Transactional
+    public Long saveItem(Item item){
+        itemRepository.save(item);
+        return item.getId();
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long id){
+        return itemRepository.findOne(id);
+    }
+}
