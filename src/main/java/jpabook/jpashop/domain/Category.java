c2102834dc5cb79883d135a5d9aecb7afe7e0d45
henoh <henoh@nkia.co.kr>
2021-08-02 10:27:24 +0900

    category 연관관계 메소드 추가

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -33,4 +33,9 @@ public class Category {
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
+    //==연관 관계 편의 메서드==//
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
