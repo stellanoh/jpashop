dbc07a64f7cd2a724eae28bc857dd7c5e96ff919
henoh <henoh@nkia.co.kr>
2021-08-02 11:37:17 +0900

    test 동작시 db 저장기능 추가

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +23,7 @@ public class MemberServiceTest {
     MemberRepository memberRepository;
 
     @Test
+    @Rollback(false) //db 저장
     public void registerTest() throws Exception {
         //given
         Member member = new Member();
