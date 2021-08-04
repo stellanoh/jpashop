fa17970fcff422bce798e405f4ddda696b4df28c
henoh <henoh@nkia.co.kr>
2021-08-04 13:25:12 +0900

    상품 목록 기능

--- src/main/java/jpabook/jpashop/controller/itemController.java
+++ src/main/java/jpabook/jpashop/controller/itemController.java
@@ -1,12 +1,15 @@
 package jpabook.jpashop.controller;
 
 import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +22,7 @@ public class itemController {
         model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
+
     @PostMapping("/items/new")
     public String create(BookForm form){
         Book book = new Book();
@@ -29,7 +33,14 @@ public class itemController {
         book.setAuthor(form.getAuthor());
 
         itemService.saveItem(book);
-        return "redirect:/";
+        return "redirect:/items";
+    }
+
+    @GetMapping("/items")
+    public String list(Model model){
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
     }
 
 
